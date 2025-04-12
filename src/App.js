072d9b194f146ca381d355672718de9502525844
adkagj <adriana.gjabelova@gmail.com>
2025-04-12 21:24:42 +0200

    fix: correct import path for Main component in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
-import Main from "./components/Main";
+import Main from "./components/Main/Main";
 import Footer from "./components/Footer";
 
 const App = () => {
