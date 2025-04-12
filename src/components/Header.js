4556c3938b7f8476de3d0092459994e84abbac72
adkagj <adriana.gjabelova@gmail.com>
2025-04-12 21:26:49 +0200

    refactor: remove unused Hero component and associated styles, clean up Header and Main components

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1>This is header</h1>
-      <h2>React</h2>
+      {/* <h1>This is header</h1>
+      <h2>React</h2> */}
     </div>
   );
 };
