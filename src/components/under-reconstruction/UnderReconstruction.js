55cd5f772c706f3e86e6e9a11def0cb041382e02
adkagj <adriana.gjabelova@gmail.com>
2025-04-26 22:40:22 +0200

    feat: add UnderReconstruction component and update navigation links

---
+++ src/components/under-reconstruction/UnderReconstruction.js
@@ -0,0 +1,15 @@
+import "./UnderReconstruction.css";
+// Under reconstruction page component
+import React from "react";
+
+const UnderReconstruction = () => {
+  return (
+    <div className="under-reconstruction">
+      <h1>🚧 Under Construction 🚧</h1>
+      <p>We're working hard to improve this page. Please check back soon!</p>
+      <div>👷‍♂️🔨</div>
+    </div>
+  );
+};
+
+export default UnderReconstruction;
